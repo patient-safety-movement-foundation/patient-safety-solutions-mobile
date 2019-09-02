343aadbe6824c95a1e3e15ca52719607b55d9486
ewolfe <e@ewolfe.me>
2019-09-02 03:00:14 -0700

    intercept external links

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import {Linking} from 'react-native';
 
 import {WebView} from 'react-native-webview';
 
-const App = () => {
-  return (
-    <WebView
-      originWhitelist={['*']}
-      source={{uri: 'https://apss.patient-safety-movement.now.sh/'}}
-    />
-  );
-};
+class App extends React.Component {
+  handleLink = newNavState => {    const {url} = newNavState;
+    if (!url) {
+      return;
+    } else if (
+      !url.startsWith('https://apss.patient-safety-movement.now.sh/')
+    ) {
+      this.webView.stopLoading();
+      Linking.openURL(newNavState.url);
+      return false;
+    }
+  };
+
+  render() {
+    return (
+      <WebView
+        originWhitelist={['*']}
+        source={{uri: 'https://apss.patient-safety-movement.now.sh/'}}
+        ref={c => {
+          this.webView = c;
+        }}
+        onNavigationStateChange={this.handleLink}
+      />
+    );
+  }
+}
 
 export default App;
